0a3c913fb0cdcd021c81295b427a03950aa1169f
losion <losion445@gmail.com>
2026-02-19 17:49:26 +0300

    [add] Spring dev container build with postgres and adminer, hot reload

---
+++ backend/src/main/java/com/github/losion445_max/backend/ClothingStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.losion445_max.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingStoreBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/losion445_max/backend/ClothingStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.losion445_max.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
