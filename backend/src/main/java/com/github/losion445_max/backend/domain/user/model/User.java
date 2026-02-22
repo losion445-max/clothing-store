22db2b4012e0543121b98c1222c5a3295f68db34
losion <losion445@gmail.com>
2026-02-22 21:30:50 +0300

    [fix] Removed useless imports

--- backend/src/main/java/com/github/losion445_max/backend/domain/user/model/User.java
+++ backend/src/main/java/com/github/losion445_max/backend/domain/user/model/User.java
@@ -3,9 +3,6 @@ package com.github.losion445_max.backend.domain.user.model;
 import java.time.Instant;
 import java.util.UUID;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
