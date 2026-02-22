a4efe76b70546d45daaf88076fac4620b0f3851b
losion <losion445@gmail.com>
2026-02-22 18:15:44 +0300

    [config] Test configuration, to make it possible i create in memroy dataase h2 and set up applicaton.yaml. Then added annotation from srping boot test to use test context

--- backend/src/test/java/com/github/losion445_max/backend/ClothingStoreBackendApplicationTests.java
+++ backend/src/test/java/com/github/losion445_max/backend/ClothingStoreBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.losion445_max.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClothingStoreBackendApplicationTests {
 
 	@Test
