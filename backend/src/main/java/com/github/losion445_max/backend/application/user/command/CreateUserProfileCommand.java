9387e7e7e727b0f5e4f930b706b0f11c17d106c2
losion <losion445@gmail.com>
2026-03-02 21:41:22 +0300

    [add] implement create user profile use caseand repository interface.

---
+++ backend/src/main/java/com/github/losion445_max/backend/application/user/command/CreateUserProfileCommand.java
@@ -0,0 +1,7 @@
+package com.github.losion445_max.backend.application.user.command;
+
+import java.util.UUID;
+
+public record CreateUserProfileCommand(UUID id) {
+    
+}
---
+++ backend/src/main/java/com/github/losion445_max/backend/application/user/dto/CreateUserProfileResult.java
@@ -0,0 +1,6 @@
+package com.github.losion445_max.backend.application.user.dto;
+
+import java.util.UUID;
+
+public record CreateUserProfileResult(UUID id) {
+}
